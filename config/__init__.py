cb343810dc0740dbeb2c9fd8577c022824ba976b
GosuOoops <111736684+GosuOoops@users.noreply.github.com>
2023-09-14 17:01:56 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -7,28 +7,28 @@ class ConfigException(Exception):
 
 def check_config():
     if not (
-        PG_DATABASE
-        and PG_USER
-        and PG_PASSWORD
-        and PG_PORT
-        and PG_HOST
+        POSTGRES_DB
+        and POSTGRES_USER
+        and POSTGRES_PASSWORD
+        and POSTGRES_PORT
+        and POSTGRES_HOST
     ):
         raise ConfigException(
             """
-            PG_DATABASE
-            PG_USER
-            PG_PASSWORD
-            PG_PORT
-            PG_HOST
+            POSTGRES_DB
+            POSTGRES_USER
+            POSTGRES_PASSWORD
+            POSTGRES_PORT
+            POSTGRES_HOST
             env vars must be set
             """
         )
 
 
-PG_PASSWORD = os.getenv("PG_PASSWORD")
-PG_HOST = os.getenv("PG_HOST")
-PG_PORT = os.getenv("PG_PORT")
-PG_USER = os.getenv("PG_USER")
-PG_DATABASE = os.getenv("PG_DATABASE")
+PG_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+PG_HOST = os.getenv("POSTGRES_HOST")
+PG_PORT = os.getenv("POSTGRES_PORT")
+PG_USER = os.getenv("POSTGRES_USER")
+PG_DATABASE = os.getenv("POSTGRES_DB")
 
 check_config()
